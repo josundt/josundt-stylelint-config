70632c29930606f5b3013d02d342c4bc66e4cf66
Jørn Andre Sundt <jorn.andre.sundt@jasmin.no>
2023-05-08 17:17:50 +0200

    Disable more rules that potentially conflict with Prettier

--- index.js
+++ index.js
@@ -16,31 +16,48 @@ module.exports = {
                     "^([a-z][a-z0-9]*)([-]{1,2}[a-z0-9]+)*$",
                     { "message": "Expected id selector to be follow BEM conventions" }
                 ],
-                "alpha-value-notation": undefined,                              // default: "percentage"        ALLOW BOTH PERCENTAGE AND NUMBER TO SPECIFY OPACITY FOR RGBA COLORS
-                "block-closing-brace-empty-line-before": undefined,             // default: "never",            ALLOW EMPTY LINE BEFORE CLOSING BRACE
-                "block-opening-brace-space-before": undefined,                  // default: "always"            ALLOW HORIZONTALLY ALIGNMENT OF RULE BLOCKS
+                "alpha-value-notation": null,                                   // default: "percentage"        ALLOW BOTH PERCENTAGE AND NUMBER TO SPECIFY OPACITY FOR RGBA COLORS
+                "block-closing-brace-empty-line-before": null,                  // default: "never",            ALLOW EMPTY LINE BEFORE CLOSING BRACE
+                "block-opening-brace-space-before": null,                       // default: "always"            ALLOW HORIZONTALLY ALIGNMENT OF RULE BLOCKS
                 "declaration-block-single-line-max-declarations": 1,            // default: 1                   MAX NUMBER OF DECLARATIONS FOR ONE LINE BLOCKS
-                "declaration-empty-line-before": undefined,                     // default: "never"             ALLOW EMOTY LINES BETWEEN DECLARATIONS
+                "declaration-empty-line-before": null,                          // default: "never"             ALLOW EMOTY LINES BETWEEN DECLARATIONS
                 "declaration-no-important": true,                               // default: false               AVOID !important
                 "function-url-quotes": "always",                                // default: null
-                "no-descending-specificity": undefined,                         // default: true                DISABLED -- REQUIRES MANY CHANGES
-                "selector-max-id": 0                                            // default: false
+                "no-descending-specificity": null,                              // default: true                DISABLED -- REQUIRES MANY CHANGES
+                "selector-max-id": 0,                                           // default: false
+
+                // ===== Disabled formatting-style related rules that potentially conflict with Prettier ====
+                "comment-empty-line-before": null,
+                "custom-property-empty-line-before": null,
+                "rule-empty-line-before": null
             }
         },
         {
             files: ["**/*.scss"],
             customSyntax: "postcss-scss",
             rules: {
-                "scss/at-import-partial-extension": undefined,                  // default: "never"             SKIP IMPORT ANALYSIS
-                "scss/dollar-variable-empty-line-before": undefined,            // default: "always+exceptions" DO NOT REQUIRE STRICT RULES FOR NEWLINES BETWEEN SASS VARIABLES  - NB! disabled formatting style related rule
-                "scss/double-slash-comment-empty-line-before": undefined,       // default: "always"            DO NOT REQUIRE NEW LINE BEFORE COMMENTS - NB! disabled formatting style related rule
-                "scss/operator-no-newline-after": undefined,                    // default: true                DISALLOW LINEBREAKS AFTER SASS OPERATORS - NB! disabled formatting style related rule
-                "scss/operator-no-newline-before": undefined,                   // default: true                DISALLOW LINEBREAKS BEFORE SASS OPERATORS - NB! disabled formatting style related rule
-                "scss/operator-no-unspaced": undefined,                         // default: true                DISALLOW LINEBREAKS BEFORE SASS OPERATORS - NB! disabled formatting style related rule
+                "scss/at-import-partial-extension": null,                       // default: "never"             SKIP IMPORT ANALYSIS
                 "scss/selector-nest-combinators": "always",                     // default: false               FORCE NESTING IN SASS
-                "scss/at-import-partial-extension": "always",                   // default: undefined           REQUIRE OR DISALLOW EXTENSION IN @IMPORT COMMANDS.
-                "scss/at-import-partial-extension-blacklist": ["scss", "sass"]  // default: undefined           SPECIFY WHITELIST OF ALLOWED FILE EXTENSIONS FOR PARTIAL NAMES IN @IMPORT COMMANDS.
+                "scss/at-import-partial-extension": "always",                   // default: null           REQUIRE OR DISALLOW EXTENSION IN @IMPORT COMMANDS.
+                "scss/at-import-partial-extension-blacklist": ["scss", "sass"],  // default: null           SPECIFY WHITELIST OF ALLOWED FILE EXTENSIONS FOR PARTIAL NAMES IN @IMPORT COMMANDS.
 
+                // ===== Disabled formatting-style related rules that potentially conflict with Prettier ====
+                "scss/at-else-closing-brace-newline-after": null,
+                "scss/at-else-closing-brace-space-after": null,
+                "scss/at-else-empty-line-before": null,
+                "scss/at-else-if-parentheses-space-before": null,
+                "scss/at-function-parentheses-space-before": null,
+                "scss/at-if-closing-brace-newline-after": null,
+                "scss/at-if-closing-brace-space-after": null,
+                "scss/at-mixin-parentheses-space-before": null,
+                "scss/dollar-variable-colon-space-after": null,
+                "scss/dollar-variable-colon-space-before": null,
+                "scss/dollar-variable-empty-line-before": null,
+                "scss/double-slash-comment-empty-line-before": null,
+                "scss/double-slash-comment-whitespace-inside": null,
+                "scss/operator-no-newline-after": null,
+                "scss/operator-no-newline-before": null,
+                "scss/operator-no-unspaced": null
             }
         }
     ]
